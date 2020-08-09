108bbf498f9065fe0c4dca9bf4e6ca5d40ec05fa
Mysher-dotcom <mike@mysher.com>
2020-08-09 17:53:16 +0800

    5.1.0.3-1

--- org.deepin.scaner/EFileApp/main.cpp
+++ org.deepin.scaner/EFileApp/main.cpp
@@ -12,12 +12,13 @@ int main(int argc, char *argv[])
 
     a.setAttribute(Qt::AA_UseHighDpiPixmaps);
     a.loadTranslator();
-    a.setOrganizationName("scaner");
-    a.setApplicationVersion(DApplication::buildVersion("5.1.0.4"));
+    a.setOrganizationName("扫描管理");
+    a.setApplicationVersion(DApplication::buildVersion("5.1.0.3-1"));
    // a.setApplicationAcknowledgementPage("http://www.viisan.com");
     a.setProductIcon(QIcon(":/img/logo/logo-16.svg"));
-    a.setProductName("scaner");
-    a.setApplicationName("scaner"); //只有在这儿修改窗口标题才有效
+    a.setProductName("扫描管理");
+    a.setApplicationName("扫描管理"); //只有在这儿修改窗口标题才有效
+    a.setApplicationDescription("扫描管理是一款便捷的扫描工具，支持多款扫描设备。");
 
     MainWindow w;
     w.show();
